1fa6da417eb8fba96fff1a8047781f7f4f394950
19weichang <k40418212@gcloud.csu.edu.tw>
2022-08-31 15:08:44 +0800

    add vite Rollup to test and public

--- vite_project/vite.config.ts
+++ vite_project/vite.config.ts
@@ -2,6 +2,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default defineConfig(({mode}) =>{
+  const flavor = mode;
+  console.log('flavor',mode);
+  const outDir = `./dist/${flavor}`;
+  return {
+    plugins: [vue(),
+    ],
+    build:{
+      chunkSizeWarningLimit:1500,//vite打包大小
+      outDir,
+      rollupOptions: { 
+        output: { //取消打包hash檔名
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`
+        }
+        // output: { //分開打包not ok
+        //   entryFileNames: '[name]-[hash].[ext]',
+        //   chunkFileNames: '[name]-[hash].[ext]',
+        //   assetFileNames: '[ext]/[name]-[hash][ext]'
+        // }
+      }  
+    },
+  }
+});
